376891c7fda1dfc64dfd2771dcefa38d2b7ca111
sanjay <sanjaykumar73189@gmail.com>
2023-02-15 00:21:08 +0530

    membership srvice

---
+++ src/main/java/com/articz/perhour/perhour/Services/MembershipServiceImpl.java
@@ -0,0 +1,58 @@
+package com.articz.perhour.perhour.Services;
+
+import com.articz.perhour.perhour.Dao.MembershipDao;
+import com.articz.perhour.perhour.Entity.Membership;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MembershipServiceImpl implements MembershipService{
+
+    @Autowired
+    public MembershipDao membershipDao;
+
+    @Override
+    public Membership add(Membership membership) {
+        membershipDao.save(membership);
+        return membership;
+    }
+
+    @Override
+    public Membership update(Membership membership) {
+        Optional<Membership> membership1=membershipDao.findById(membership.getId());
+        if(membership1.isPresent()){
+            Membership membership2=membership1.get();
+            membership2=membership;
+            membershipDao.save(membership2);
+            return membership2;
+        }
+
+        return null;    }
+
+
+    @Override
+    public Membership delete(long id) {
+        Optional<Membership> membership1=membershipDao.findById(id);
+        if(membership1.isPresent()){
+            membershipDao.deleteById(id);
+            return membership1.get();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Membership> readall() {
+        return membershipDao.findAll();
+    }
+
+    @Override
+    public Membership read(long id) {
+        Optional<Membership> membership1=membershipDao.findById(id);
+        if(membership1.isPresent()){
+
+            return membership1.get();
+        }
+        return null;
+    }
+}
